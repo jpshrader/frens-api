7b2a278c2184df20135ae7b7eee9b080b2ee34f8
jpshrader <46694288+jpshrader@users.noreply.github.com>
2021-02-02 16:58:35 -0500

    Formatting

--- frens-api/Endpoints/V1/FrenController.cs
+++ frens-api/Endpoints/V1/FrenController.cs
@@ -1,9 +1,5 @@
 ﻿using frens_api.Common;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace frens_api.Endpoints.V1 {
 	[ApiController]
